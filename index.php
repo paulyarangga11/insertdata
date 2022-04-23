b41f8824ed7589eb81de1cecddd1750994f520ef
paulyarangga11 <paulyarangga07@gmail.com>
2022-04-23 10:48:48 +0900

    add file index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="first" placeholder="First Name">
+        <br>
+        <input type="text" name="last" placeholder="Last Name">
+        <br>
+        <input type="text" name="email" placeholder="E-mail">
+        <br>
+        <input type="text" name="uid" placeholder="Username">
+        <br> 
+        <input type="text" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit" name="submit">Sign up</button>
+    </form>
+</body>
+</html>
